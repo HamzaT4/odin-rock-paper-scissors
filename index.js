4f5e2411cdfb7b86dfd28d56b8f16c4f91b8adc6
HamzaT4 <hamzatay2012@gmail.com>
2022-08-30 20:35:35 +0300

    Design UI.

--- index.js
+++ index.js
@@ -1,30 +1,77 @@
-let userChoice = prompt("Rock,Paper,Scissors Shot!(R,P,S)");
-
-
+let  champions = document.querySelectorAll('.imgs img');
+let cpuScore= document.querySelector('.cpu-score');
+let userScore=document.querySelector('.user-score');
 let cpuChoice = Math.floor(Math.random()*3)+1;
+let result = document.querySelector('.container');
+champions.forEach(champion => champion.addEventListener("click",
+playRound ));
 
-switch (cpuChoice) {
-    case 1:
-        alert("The Computer played Rock!")
-        if(userChoice=='R')  alert("Draw!"); 
-        else if(userChoice=='P')  alert("You win!");
-        else if (userChoice=='S')  alert("You lose!");
-        
-        break;
+function playRound(pickedChamp) {
+    let userChoice = pickedChamp.target.alt;
+    let cpuChoice = Math.floor(Math.random()*3)+1;
+    switch (cpuChoice) {
+        case 1:
+          
+            if(userChoice=='rock')  alert("The Bot Picked Malphit Too Draw!"); 
+            else if(userChoice=='paper'){
+                alert("The Bot Picked Malphit You win!");
+                userScore.textContent++;
 
-   case 2:
-        alert("The Computer played Paper!")
-        if(userChoice=='R')  alert("You lose!"); 
-        else if(userChoice=='P')  alert("Draw!");
-        else if (userChoice=='S')  alert("You win!");
-        
-        break;
-    case 3:
-        alert("The Computer played Scissor!")
-        if(userChoice=='R')  alert("You win!"); 
-        else if(userChoice=='P')  alert("You lose!");
-        else if (userChoice=='S')  alert("Draw!");
-        
-        break;
-        default:alert("ERROR!")
+            }
+            else if (userChoice=='scissor'){
+                alert("The Bot Picked Malphit You lose!");
+                cpuScore.textContent++;  
+            }  
+            
+            break;
+    
+       case 2:
+            
+            if(userChoice=='rock') {
+                alert("The Bot Picked Twisted Fate You lose!");
+                cpuScore.textContent++;   
+            } 
+            else if(userChoice=='paper')  alert("The Bot Picked Twisted Fate Draw!");
+            else if (userChoice=='scissor') {
+                alert("The Bot Picked Twisted Fate You win!");
+                userScore.textContent++;
+            } 
+            
+            break;
+        case 3:
+           
+            if(userChoice=='rock'){
+                alert("The Bot Picked Gwen You Win!");
+                userScore.textContent++;
+            }   
+            else if(userChoice=='paper') {
+                alert("The Bot Picked Gwen You lose!");
+                cpuScore.textContent++;
+            } 
+
+            else if (userChoice=='scissor')  alert("The Bot Picked Gwen Draw!");
+            
+            break;
+            default:alert("ERROR!")
+    }
+    
+if (cpuScore.textContent==3) defeat();
+    
+
+if(userScore.textContent == 3) victory();
 }
+
+
+function defeat(){
+   
+    result.innerHTML =
+     '<img style = "width : 500px;" src="/images/lose.png" alt="">  <h2>Click here to start a new game </h2> ';
+    document.querySelector("h2").addEventListener("click", ()=> location.reload());
+}
+
+function victory(){
+    result.innerHTML = 
+    '<img style = "width : 500px;" src="/images/win.png" alt="">  <h2>Click here to start a new game </h2> ';
+    document.querySelector("h2").addEventListener("click", ()=> location.reload());
+}
+
